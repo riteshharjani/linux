56d0b6ed71fdb9efce08481fb280843912a4dc76
Zhang Yi <yi.zhang@huawei.com>
2024-01-27 09:58:09 +0800

    ext4: correct delalloc extent length
    When adding a delalloc extent in ext4_da_map_blocks(), the extent
    length can be incorrect if we found a cached hole extent entry and the
    hole length is smaller than the origin map->m_len. Fortunately, it
    should not be able to trigger any issue now because the map->m_len is
    always 1. Fix this by adjust length before inserting delalloc extent.
    
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -1712,6 +1712,11 @@ static int ext4_da_map_blocks(struct inode *inode, sector_t iblock,
 
 	/* Lookup extent status tree firstly */
 	if (ext4_es_lookup_extent(inode, iblock, NULL, &es)) {
+		retval = es.es_len - (iblock - es.es_lblk);
+		if (retval > map->m_len)
+			retval = map->m_len;
+		map->m_len = retval;
+
 		if (ext4_es_is_hole(&es))
 			goto add_delayed;
 
@@ -1727,10 +1732,6 @@ static int ext4_da_map_blocks(struct inode *inode, sector_t iblock,
 		}
 
 		map->m_pblk = ext4_es_pblock(&es) + iblock - es.es_lblk;
-		retval = es.es_len - (iblock - es.es_lblk);
-		if (retval > map->m_len)
-			retval = map->m_len;
-		map->m_len = retval;
 		if (ext4_es_is_written(&es))
 			map->m_flags |= EXT4_MAP_MAPPED;
 		else if (ext4_es_is_unwritten(&es))
