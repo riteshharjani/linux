47efa458e318279c3beae26953361903f87db4fd
Ojaswin Mujoo <ojaswin@linux.ibm.com>
2024-08-20 14:57:08 +0530

    ext4: Convert EXT4_B2C(sbi->s_stripe) users to EXT4_NUM_B2C
    Although we have checks to make sure s_stripe is a multiple of cluster
    size, in case we accidentally end up with a scenario where this is not
    the case, use EXT4_NUM_B2C() so that we don't end up with unexpected
    cases where EXT4_B2C(stripe) becomes 0.
    
    Also make the is_stripe_aligned check in regular_allocator a bit more
    robust while we are at it. This should ideally have no functional change
    unless we have a bug somewhere causing (stripe % cluster_size != 0)
    
    Signed-off-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>
    Reviewed-by: Kemeng Shi <shikemeng@huaweicloud.com>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -2553,7 +2553,7 @@ void ext4_mb_scan_aligned(struct ext4_allocation_context *ac,
 	do_div(a, sbi->s_stripe);
 	i = (a * sbi->s_stripe) - first_group_block;
 
-	stripe = EXT4_B2C(sbi, sbi->s_stripe);
+	stripe = EXT4_NUM_B2C(sbi, sbi->s_stripe);
 	i = EXT4_B2C(sbi, i);
 	while (i < EXT4_CLUSTERS_PER_GROUP(sb)) {
 		if (!mb_test_bit(i, bitmap)) {
@@ -2928,9 +2928,11 @@ repeat:
 			if (cr == CR_POWER2_ALIGNED)
 				ext4_mb_simple_scan_group(ac, &e4b);
 			else {
-				bool is_stripe_aligned = sbi->s_stripe &&
+				bool is_stripe_aligned =
+					(sbi->s_stripe >=
+					 sbi->s_cluster_ratio) &&
 					!(ac->ac_g_ex.fe_len %
-					  EXT4_B2C(sbi, sbi->s_stripe));
+					  EXT4_NUM_B2C(sbi, sbi->s_stripe));
 
 				if ((cr == CR_GOAL_LEN_FAST ||
 				     cr == CR_BEST_AVAIL_LEN) &&
@@ -3707,7 +3709,7 @@ int ext4_mb_init(struct super_block *sb)
 	 */
 	if (sbi->s_stripe > 1) {
 		sbi->s_mb_group_prealloc = roundup(
-			sbi->s_mb_group_prealloc, EXT4_B2C(sbi, sbi->s_stripe));
+			sbi->s_mb_group_prealloc, EXT4_NUM_B2C(sbi, sbi->s_stripe));
 	}
 
 	sbi->s_locality_groups = alloc_percpu(struct ext4_locality_group);
