325232639f8bd427135ec4d1a0956e09ed87ca77
Ritesh Harjani (IBM) <ritesh.list@gmail.com>
2024-10-20 15:43:45 +0530

    ext4: Add statx support for atomic writes
    This patch adds base support for atomic writes via statx getattr.
    On bs < ps systems, we can create FS with say bs of 16k. That means
    both atomic write min and max unit can be set to 16k for supporting
    atomic writes.
    
    Co-developed-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>
    Signed-off-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>
    Signed-off-by: Ritesh Harjani (IBM) <ritesh.list@gmail.com>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -1729,6 +1729,10 @@ struct ext4_sb_info {
 	 */
 	struct work_struct s_sb_upd_work;
 
+	/* Atomic write unit values in bytes */
+	unsigned int s_awu_min;
+	unsigned int s_awu_max;
+
 	/* Ext4 fast commit sub transaction ID */
 	atomic_t s_fc_subtid;
 
@@ -3853,6 +3857,11 @@ static inline int ext4_buffer_uptodate(struct buffer_head *bh)
 	if (buffer_write_io_error(bh))
 		set_buffer_uptodate(bh);
 	return buffer_uptodate(bh);
+}
+
+static inline bool ext4_can_atomic_write(struct super_block *sb)
+{
+	return EXT4_SB(sb)->s_awu_min > 0;
 }
 
 extern int ext4_block_write_begin(handle_t *handle, struct folio *folio,
