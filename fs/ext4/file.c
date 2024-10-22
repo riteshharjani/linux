e6ac9b22d9ab557c9bcac9189c0c5992ecb5ce06
Ritesh Harjani (IBM) <ritesh.list@gmail.com>
2024-10-22 19:10:44 +0530

    ext4: Warn if we ever fallback to buffered-io for DIO atomic writes
    iomap will not return -ENOTBLK in case of dio atomic writes. But let's
    also add a WARN_ON_ONCE as a safety net.
    
    Signed-off-by: Ritesh Harjani (IBM) <ritesh.list@gmail.com>

--- fs/ext4/file.c
+++ fs/ext4/file.c
@@ -576,8 +576,15 @@ static ssize_t ext4_dio_write_iter(struct kiocb *iocb, struct iov_iter *from)
 		iomap_ops = &ext4_iomap_overwrite_ops;
 	ret = iomap_dio_rw(iocb, from, iomap_ops, &ext4_dio_write_ops,
 			   dio_flags, NULL, 0);
-	if (ret == -ENOTBLK)
+	if (ret == -ENOTBLK) {
 		ret = 0;
+		/*
+		 * iomap will never return -ENOTBLK if write fails for atomic
+		 * write. But let's just add a safety net.
+		 */
+		WARN_ON_ONCE(iocb->ki_flags & IOCB_ATOMIC);
+	}
+
 	if (extend) {
 		/*
 		 * We always perform extending DIO write synchronously so by
