bebb6c79724ab5b05cf9fe05d0996d5d477dc454
Gregory Price <gourry@gourry.net>
2025-01-06 19:03:42 -0500

    memory: move conditionally defined enums use inside ifdef tags
    NUMA_HINT_FAULTS and NUMA_HINT_FAULTS_LOCAL are only defined if
    CONFIG_NUMA_BALANCING is defined, but are used outside the tags in
    numa_migrate_check().  Fix this.
    
    TNF_SHARED is only used if CONFIG_NUMA_BALANCING is enabled, so
    moving this line inside the ifdef is also safe - despite use of TNF_*
    elsewhere in the function.  TNF_* are not conditionally defined.
    
    Signed-off-by: Gregory Price <gourry@gourry.net>

--- mm/memory.c
+++ mm/memory.c
@@ -5571,14 +5571,14 @@ int numa_migrate_check(struct folio *folio, struct vm_fault *vmf,
 	/* Record the current PID acceesing VMA */
 	vma_set_access_pid_bit(vma);
 
-	count_vm_numa_event(NUMA_HINT_FAULTS);
 #ifdef CONFIG_NUMA_BALANCING
+	count_vm_numa_event(NUMA_HINT_FAULTS);
 	count_memcg_folio_events(folio, NUMA_HINT_FAULTS, 1);
-#endif
 	if (folio_nid(folio) == numa_node_id()) {
 		count_vm_numa_event(NUMA_HINT_FAULTS_LOCAL);
 		*flags |= TNF_FAULT_LOCAL;
 	}
+#endif
 
 	return mpol_misplaced(folio, vmf, addr);
 }
