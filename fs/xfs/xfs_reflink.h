77a316952b3d26c5f2eb0e52651a88b66178f95d
John Garry <john.g.garry@oracle.com>
2025-03-03 17:11:09 +0000

    xfs: Pass flags to xfs_reflink_allocate_cow()
    In future we will want more boolean options for xfs_reflink_allocate_cow(),
    so just prepare for this by passing a flags arg for @convert_now.
    
    Suggested-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: John Garry <john.g.garry@oracle.com>

--- fs/xfs/xfs_reflink.h
+++ fs/xfs/xfs_reflink.h
@@ -7,6 +7,11 @@
 #define __XFS_REFLINK_H 1
 
 /*
+ * Flags for xfs_reflink_allocate_cow()
+ */
+#define XFS_REFLINK_CONVERT	(1u << 0) /* convert unwritten extents now */
+
+/*
  * Check whether it is safe to free COW fork blocks from an inode. It is unsafe
  * to do so when an inode has dirty cache or I/O in-flight, even if no shared
  * extents exist in the data fork, because outstanding I/O may target blocks
@@ -32,7 +37,7 @@ int xfs_bmap_trim_cow(struct xfs_inode *ip, struct xfs_bmbt_irec *imap,
 
 int xfs_reflink_allocate_cow(struct xfs_inode *ip, struct xfs_bmbt_irec *imap,
 		struct xfs_bmbt_irec *cmap, bool *shared, uint *lockmode,
-		bool convert_now);
+		unsigned int flags);
 extern int xfs_reflink_convert_cow(struct xfs_inode *ip, xfs_off_t offset,
 		xfs_off_t count);
 
